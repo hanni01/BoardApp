700e90e72c5af211ad1f69396d1c628e82269f98
hanni01 <he3080@gmail.com>
2022-12-30 18:59:05 +0900

    DB connection

--- src/main/java/com/bootcamp/controller/HomeController.java
+++ src/main/java/com/bootcamp/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.bootcamp.myproject;
+package com.bootcamp.controller;
 
 import java.text.DateFormat;
 import java.util.Date;
@@ -11,18 +11,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
+
 @Controller
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	
+	@RequestMapping(value = "/home", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
---
+++ src/main/java/com/bootcamp/dao/BoardDAOImpl.java
@@ -0,0 +1,39 @@
+package com.bootcamp.dao;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import com.bootcamp.domain.BoardVO;
+
+@Repository
+public class BoardDAOImpl implements BoardDAO{
+	@Inject
+	private SqlSession session;
+	private static String namespace = "com.mapper.BoardMapper";
+	
+	//namespace가 com.mapper.BoardMapper인 곳의 id 찾아 매개변수 전달
+	@Override
+	public void create(BoardVO vo) throws Exception {
+		session.insert(namespace+".create", vo);
+	}
+	@Override
+	public BoardVO read(Integer bno) throws Exception {
+		return session.selectOne(namespace+".read", bno);
+	}
+	@Override
+	public void update(BoardVO vo) throws Exception {
+		session.update(namespace+".update", vo);
+	}
+	@Override
+	public void delete(Integer bno) throws Exception {
+		session.delete(namespace+".delete", bno);
+	}
+	@Override
+	public List<BoardVO> listAll() throws Exception {
+		return session.selectList(namespace+".listAll");
+	}
+}

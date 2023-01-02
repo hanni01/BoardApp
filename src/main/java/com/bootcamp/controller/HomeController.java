4d0cb6489a829ae87afc8701080cd430e54fc9a8
hanni01 <he3080@gmail.com>
2023-01-02 23:23:18 +0900

    fix some information

--- src/main/java/com/bootcamp/controller/HomeController.java
+++ src/main/java/com/bootcamp/controller/HomeController.java
@@ -18,7 +18,7 @@ public class HomeController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	
-	@RequestMapping(value = "/home", method = RequestMethod.GET)
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -1,5 +1,6 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-<%@ page session="false" %>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<%@ page session=false %>
 <html>
 <head>
 	<title>Home</title>
